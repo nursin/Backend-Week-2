7b3781cd0ff2abce11c138916e856dc19f0b6d17
Bobby Keel <bobbykeel76@gmail.com>
2021-08-31 08:51:33 -0500

    setup express server app.js, added routers, and html pages in public

---
+++ nucampsiteServer/routes/partnerRouter.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const partnerRouter = express.Router();
+
+partnerRouter.route('/')
+.all((req, res, next) => {
+  res.statusCode = 200;
+  res. setHeader('Content-Type', 'text/html');
+  next();
+})
+.get((req, res) => {
+  res.end('Will send all the partners to you');
+})
+.post((req, res) => {
+  res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /partners');
+})
+.delete((req, res) => {
+  res.end('Deleting all partners');
+});
+
+partnerRouter.route('/:partnerId')
+.all((req, res, next) => {
+  res.statusCode = 200;
+  res. setHeader('Content-Type', 'text/html');
+  next();
+})
+.get((req, res) => {
+  res.end(`Will send details of the campsite: ${req.params.partnerId} to you`);
+})
+.post((req, res) => {
+  res.statusCode = 403;
+  res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
+})
+.put((req, res) => {
+  res.write(`Updating the campsite: ${req.params.partnerId}\n`)
+  res.end(`Will update the campsite: ${req.body.name} 
+      with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+  res.end(`Deleting campsite: ${req.params.partnerId}`);
+});
+
+module.exports = partnerRouter;
